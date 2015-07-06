1fe845ca6ccdfd7d50b1875a73f536520dca22a5
Olivia Cheng <olivia.cheng14@gmail.com>
2015-07-05 23:06:14 -0700

    http server

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var fs = require('fs');
-
+var http = require('http');
 var data = fs.readFileSync(__dirname + '/data.txt', 'utf8');
 
+var server = http.createServer(function(req, res) {
+  var data = fs.readFileSync(__dirname + '/data.txt', 'utf8');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.write(data);
+  res.end();
+});
+
+server.listen(8080);
+
 console.log(data);
+
+console.log('listening on port 8080');
